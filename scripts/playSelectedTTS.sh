dd24e06ade0ce2b84aec31c1116e8350a6dc4432
Yazan Agha-Schrader <mountaiin@icloud.com>
2024-05-14 08:36:17 +0200

    add german voice & fix xclip selection
    - add thorsten voice
    - use xclip -out -selection primary
    
    dependency:
    - use play from sox lib instead of aplay

--- scripts/playSelectedTTS.sh
+++ scripts/playSelectedTTS.sh
@@ -1,17 +1,24 @@
-#!/bin/bash 
+#!/bin/bash
 
 set -eo pipefail
 
 dir="$(dirname $0)"
-input="/tmp/toplay.$RANDOM-$RANDOM"
-wav="/tmp/toplay.$RANDOM-$RANDOM.wav"
+model="$dir/thorsten-hessisch.onnx"
 
-data=$(xclip -out selection)
+# Debugging: Ausgabe des Verzeichnisses und Modells
+echo "Verzeichnis: $dir"
+echo "Modell: $model"
+
+# Verwende die primäre Auswahl, um den aktuell markierten Text zu erhalten
+data=$(xclip -out -selection primary)
+# Debugging: Ausgabe der Länge der Daten
+echo "Länge der Daten: ${#data}"
+
 if [ ${#data} -gt 2 ]; then
-    echo $data > $input
-    echo "$(date) data: $input" >> /tmp/piper.log &
-    "$dir"/piper --model "$dir"/en_US-amy-low.onnx --output_file $wav < $input &
-    wait
-    aplay $dir/silence.wav $wav
-    rm $input $wav || : 
+    # Debugging: Ausgabe der Daten
+    echo "Daten: $data"
+    echo "$(date) data: $data" >> /tmp/piper.log &
+    echo "$data" | "$dir"/piper --model "$model" --output_raw | play -t raw -r 22050 -e signed-integer -b 16 -
+else
+    echo "Keine ausreichenden Daten zum Vorlesen."
 fi
